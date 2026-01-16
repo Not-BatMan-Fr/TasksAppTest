22cef7a7b8d3e77a59d10d7ea654c59635ad0ed0
MriteshAdak <mriteshadak@outlook.com>
2026-01-16 21:32:24 +0100

    updated import package for declarative base

--- src/main.py
+++ src/main.py
@@ -1,8 +1,7 @@
 import uuid
 from fastapi import FastAPI
 from sqlalchemy import create_engine, Column, String
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import declarative_base, sessionmaker
 from pydantic import BaseModel
 
 # --- DATABASE CONFIGURATION ---
